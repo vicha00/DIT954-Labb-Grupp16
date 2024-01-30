988682cc24c1a0f6a61d3873ce7fc16c3b3fcb85
MindokRuvaak <cole_daniel@rocketmail.com>
2024-01-30 16:33:11 +0100

    tests done, made vehicle engine and turnign more realistic, parent vehicle of saab and volvo now final and named vehicle

---
+++ test/Saab95Test.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Saab95Test {
+    Saab95 testSaab;
+
+    @Before
+    public void init() {
+        testSaab = new Saab95();
+        testSaab.startEngine();
+    }
+
+    @Test
+    public void testTurbo() {
+        // turbo off 
+        testSaab.gas(1);
+        double gasWoTurbo = testSaab.getCurrentSpeed();
+        testSaab.setTurboOn();
+        testSaab.gas(1);
+        double gasWithTurbo = testSaab.getCurrentSpeed();
+        assertTrue(gasWithTurbo > gasWoTurbo);
+    }
+}
---
+++ test/Volvo240Test.java
@@ -0,0 +1,4 @@
+public class Volvo240Test {
+    // all volvo240 methods are deligated to vehicle 
+    // for tests: see VehicleTest
+}
