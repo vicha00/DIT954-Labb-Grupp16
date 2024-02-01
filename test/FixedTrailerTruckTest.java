cb2abb15b3ce642c2076b5172af7ebdcce5b286a
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-02-01 16:04:30 +0100

    added tests for FixedTrailerTruck and VolvoFL

---
+++ test/FixedTrailerTruckTest.java
@@ -0,0 +1,31 @@
+import org.junit.*;
+import java.awt.*;
+import static org.junit.Assert.*;
+
+public class FixedTrailerTruckTest {
+
+    FixedTrailerTruck<NormalCar> testTruck;
+    NormalCar dummy;
+    @Before
+    public void init() {
+        testTruck = new FixedTrailerTruck<>(200, Color.GREEN,"VolvoFL");
+        dummy = new NormalCar(0,Color.BLACK,"dummy");
+    }
+
+    @Test
+    public void storeAndRemoveTest() {
+        testTruck.storeThing(dummy);
+        assertEquals("Storage not open add",0, testTruck.countThings());
+        testTruck.openStorage();
+        testTruck.storeThing(dummy);
+        assertEquals("Storage open add",1,testTruck.countThings());
+        testTruck.closeStorage();
+        NormalCar car = testTruck.removeThing();
+        assertEquals("storage not open remove", 1, testTruck.countThings());
+        assertNull("storage not open remove",car);
+        testTruck.openStorage();
+        car = testTruck.removeThing();
+        assertEquals("storage open remove",0,testTruck.countThings());
+        assertNotNull("storage open remove",car);
+    }
+}
