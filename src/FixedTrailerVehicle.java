f82bbd680dec7f9b7baadb126d6a2363b25af99f
MindokRuvaak <cole_daniel@rocketmail.com>
2024-02-01 14:16:27 +0100

    still pain

---
+++ src/FixedTrailerVehicle.java
@@ -0,0 +1,48 @@
+import java.awt.Color;
+import java.util.Stack;
+
+public class FixedTrailerVehicle extends Vehicle implements  HasStorage<Vehicle> {
+    // TODO : change vehicle to car
+
+    public FixedTrailerVehicle(int nrDoors, double enginePower, Color color, String modelName) {
+        super(nrDoors, enginePower, color, modelName);
+        Stack<Vehicle> storage = new Stack<>();
+    }
+
+    @Override
+    public void openStorage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'openStorage'");
+    }
+
+    @Override
+    public void closeStorage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'closeStorage'");
+    }
+
+    @Override
+    public boolean isStorageOpen() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isStorageOpen'");
+    }
+
+    @Override
+    public void storeThing(Vehicle toStore) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'storeThing'");
+    }
+
+    @Override
+    public Vehicle removeThing() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeThing'");
+    }
+
+    @Override
+    public int countThings() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'countThings'");
+    }
+
+}
---
+++ src/VolvoFL.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class VolvoFL extends FixedTrailerVehicle {
+
+    public VolvoFL(int nrDoors, double enginePower, Color color, String modelName) {
+        super(nrDoors, enginePower, color, modelName);
+        //TODO Auto-generated constructor stub
+    }
+
+}
