3c7d16a49e598c874825a0045bdf063ab3262a31
Nikhil Olsson Mukhopadhyay <gusnikhol@gu.student.se>
2024-02-02 11:22:51 +0100

    done!

--- src/ScaniaV8.java
+++ src/ScaniaV8.java
@@ -6,7 +6,7 @@ public class ScaniaV8<T> extends Truck<T> implements Tippable<T>{
     private double storageAngle;
     public ScaniaV8() {
         super(200, Color.white, "ScaniaV8");
-        this.storageAngle = 0;
+        this.storageAngle = 0.0;
     }
 
     @Override
@@ -15,6 +15,9 @@ public class ScaniaV8<T> extends Truck<T> implements Tippable<T>{
     }
 
     private void setStorageAngle(double angle) {
+        if(Math.abs(getCurrentSpeed()) > 0.01) {
+            return;
+        }
         storageAngle = Math.clamp(angle,0,70);
     }
 
@@ -29,7 +32,10 @@ public class ScaniaV8<T> extends Truck<T> implements Tippable<T>{
     }
 
     @Override
-    public void emptyStorage() {
-
+    public void gas(double ammount) {
+        if(Math.abs(storageAngle) > 0.01) {
+            return;
+        }
+        super.gas(ammount);
     }
 }
--- test/ScaniaV8Test.java
+++ test/ScaniaV8Test.java
@@ -1,3 +1,40 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class ScaniaV8Test {
-    
+
+    ScaniaV8<Object> testTSP;
+
+    @Before
+    public void init() {
+        this.testTSP = new ScaniaV8<>();
+        this.testTSP.startEngine();
+    }
+
+     @Test
+    public void lowerAndHeightenStorage() {
+        testTSP.raiseStorage(70);
+        testTSP.lowerStorage(35);
+        assertEquals(35, testTSP.getStorageAngle(), 0.01);
+        testTSP.lowerStorage(35);
+        assertNotEquals(35, testTSP.getStorageAngle(), 0.01);
+        testTSP.lowerStorage(80);
+        assertEquals(0, testTSP.getStorageAngle(), 0.01);
+        testTSP.raiseStorage(70);
+        assertEquals(70, testTSP.getStorageAngle(), 0.01);
+        testTSP.lowerStorage(70);
+        testTSP.gas(1);
+        testTSP.raiseStorage(70);
+        assertNotEquals(70, testTSP.getStorageAngle(), 0.01);
+        testTSP.brake(1);
+        testTSP.raiseStorage(80);
+        assertEquals(70, testTSP.getStorageAngle(), 0.01);
+    }
+
+    @Test
+    public void noGasWhenStorageIsUp (){
+        testTSP.raiseStorage(70);
+        testTSP.gas(1);
+        assertEquals(0, testTSP.getCurrentSpeed(), 0.01);
+    }
 }
