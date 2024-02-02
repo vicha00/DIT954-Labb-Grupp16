058474f17fed35f92f26d9af6110973a016f953f
MindokRuvaak <cole_daniel@rocketmail.com>
2024-02-02 12:55:14 +0100

    tests

--- test/VolvoFLTest.java
+++ test/VolvoFLTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.awt.geom.Point2D;
+
 public class VolvoFLTest {
     VolvoFL testTSP;
 
@@ -7,6 +9,18 @@ public class VolvoFLTest {
     public void init() {
         this.testTSP = new VolvoFL();
         this.testTSP.startEngine();
+    }
+
+    @Test 
+    public void testStoreThing(){
+        Volvo240 car1 = new Volvo240();
+        Saab95 car2 = new Saab95();
+        testTSP.setPosition(new Point2D.Double(3, 3));
+        testTSP.storeThing(car1);
+        assertEquals(0, testTSP.countThings());
+        car2.setPosition(new Point2D.Double(3,3));
+        testTSP.storeThing(car2);
+        assertEquals(1, testTSP.countThings());
     }
 
     @Test
